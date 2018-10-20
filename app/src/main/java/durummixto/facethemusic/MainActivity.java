fe5221707d35f24efa857c0447ba765056cdd6b8
J.T. Parrish <jparrish34@gatech.edu>
2018-10-20 01:42:35 -0400

    Started fleshing out camera usage

--- app/src/main/java/durummixto/facethemusic/MainActivity.java
+++ app/src/main/java/durummixto/facethemusic/MainActivity.java
@@ -1,7 +1,13 @@
 package durummixto.facethemusic;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.camera2.CameraManager;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CameraManager c = getBaseContext().getSystemService(CameraManager.class);
+        String[] camIdList = null;
+        try {
+            camIdList = c.getCameraIdList();
+        } catch (CameraAccessException e) {
+            Log.println(6, "camera access", "OH SHIT");
+        }
+        //c.openCamera(camIdList[0],);
     }
 }
