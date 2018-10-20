3ce57c8cb198c2c6a2669749eb09cef7eaf5dcd4
zday123 <zday123@yahoo.com>
2018-10-20 10:50:39 -0400

    Have a working MainActivity.java and LoginActivity.java that recognizes com.spotify

--- app/src/main/java/durummixto/facethemusic/MainActivity.java
+++ app/src/main/java/durummixto/facethemusic/MainActivity.java
@@ -1,14 +1,21 @@
 package durummixto.facethemusic;
 
-<<<<<<< HEAD
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.spotify.android.appremote.*;
-import com.spotify.protocol.*;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 
-=======
 import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
 import android.support.v7.app.AppCompatActivity;
@@ -17,13 +24,19 @@ import android.hardware.camera2.CameraManager;
 import android.util.Log;
 
 import java.util.List;
->>>>>>> fe5221707d35f24efa857c0447ba765056cdd6b8
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CLIENT_ID = "dc8addce37f04b198a0ac367af5964ed";
     private static final String REDIRECT_URI = "com.durummixto.facethemusic://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
+
+    private static final int REQUEST_CODE = 1337;
+
+    
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
