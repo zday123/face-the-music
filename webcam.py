fdc6f3f0cc460a8fc090a73e63e6527ad6559b26
Tyler <tsmith394@gatech.edu>
2018-10-21 01:45:44 -0400

    recommit webcam.py

--- webcam.py
+++ webcam.py
@@ -1,6 +1,9 @@
 import cv2
+import os, inspect
 
-cam = cv2.namedWindow("test")
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
 
 img_counter = 0
 
@@ -12,15 +15,18 @@ while True:
     k = cv2.waitKey(1)
 
     if k%256 == 27:
-        #Escape key pressed
-        print("Escape key pressed, camera closing")
+        # ESC pressed
+        print("Escape hit, closing...")
         break
-    elif k % 256 == 32:
-        # spacebar pressed
+    elif k%256 == 32:
+        # SPACE pressed
         img_name = "opencv_frame_{}.png".format(img_counter)
+        #print inspect.getfile(inspect.currentframe()) # script file name
+        #print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
-        
+
 cam.release()
+
 cv2.destroyAllWindows()
